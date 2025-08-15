d4413fc43f61ce1693e0bdc4f7e3dccbec0d9919
Mikey Dowsett <mikeydowsett@proton.me>
2025-08-14 23:06:02 -0500

    Improved 404

--- src/pages/Support Pages/ErrorNotFound.vue
+++ src/pages/Support Pages/ErrorNotFound.vue
@@ -1,24 +1,7 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
-    </div>
+  <div class="absolute-center" style="text-align: center; display: flex; flex-direction: column;">
+    <q-img src="images/404.svg" style="width: 600px; height: auto" />
+    <q-btn label="Home" to="/" class="submit q-mt-lg" color="primary" />
   </div>
 </template>
 
